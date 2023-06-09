e65f44060ad638fcbcdc0f095f07417af1b05dd4
Christian <ChristianQuidet07@gmail.com>
2023-06-09 21:02:09 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,92 @@
+let menuBtn = document.querySelector("#menu-btn");
+let navbar = document.querySelector(".header .flex .navbar");
+
+menuBtn.onclick = () => {
+  menuBtn.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
+
+const courseSwiper = new Swiper(".course-slider", {
+  spaceBetween: 20,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+const instructorSwiper = new Swiper(".instructor-slider", {
+  spaceBetween: 20,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+const reviewsSwiper = new Swiper(".reviews-slider", {
+  spaceBetween: 20,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+const homeSwiper = new Swiper(".image", {
+  spaceBetween: 20,
+  grabCursor: true,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 1,
+    },
+    // 768: {
+    //   slidesPerView: 2,
+    // },
+    // 1024: {
+    //   slidesPerView: 3,
+    // },
+  },
+});
